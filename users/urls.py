11c51721f07dd6757f96244b28e44eed46bff516
skyfather <noikofi.nk@gmail.com>
2020-07-20 19:18:52 +0300

    Added account Profile view and urlpattern

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from . views import SignupView,StudentSignUpView,TeacherSignUpView
+from . views import SignupView,StudentSignUpView,TeacherSignUpView, ProfileUpdateView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('signup/student/', StudentSignUpView.as_view(), name='student_signup'),
     path('signup/teacher/', TeacherSignUpView.as_view(), name='teacher_signup'),
+    path('profile/<int:pk>/', ProfileUpdateView.as_view(), name='profile'),
 ]
