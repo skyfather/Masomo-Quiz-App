35587fb44de05960dd14373bba4eff1782112673
skyfather <noikofi.nk@gmail.com>
2020-07-23 19:18:49 +0300

    Added generic views for detail, update and delete

--- learning/views/answers.py
+++ learning/views/answers.py
@@ -1,6 +1,6 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .. models import Answer
@@ -18,8 +18,24 @@ class CreateAnswerView(CreateView):
     # success_url = reverse_lazy('quiz_detail')
     template_name = 'learning\\create_answer.html'
     success_url = reverse_lazy('question_list')
+    # success_url = reverse_lazy('question_detail')
     # success_url = reverse_lazy('question_detail', args=[self.question.pk])
     # def get(self, request, *args,**kwargs):
     #     print("----------------------------------------")
     #     print(request)
     #     print("----------------------------------------")
+
+class AnswerDetailView(DetailView):
+    model = Answer
+    template_name = 'learning\\answer_detail.html'
+    
+@method_decorator([login_required, teacher_required], name='dispatch')
+class AnswerUpdateView(UpdateView):
+    model = Answer
+    template_name = 'learning\\create_answer.html'
+    fields = '__all__'
+
+@method_decorator([login_required, teacher_required], name='dispatch')
+class AnswerDeleteView(DeleteView):
+    model = Answer
+    success_url = reverse_lazy('quiz_list')
